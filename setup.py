509cc5c9e873f06d2d910194be9b4eb2467d1dbd
Paris Kasidiaris <paris@sourcelair.com>
2017-11-23 15:55:57 +0200

    Change structure

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='cabinboy',
-    version='0.1.0',
+    version='0.2.0',
     description='run tasks in your code base with astounding easiness.',
     url='https://github.com/parisk/cabinboy',
     author='Paris Kasidiaris',
     author_email='paris@sourcelair.com',
     license='MIT',
+    packages=find_packages(),
     entry_points={
-        'console_scripts': ['cb=cb:run'],
+        'console_scripts': ['cb=cabinboy.cli:run'],
     },
     install_requires=[
         'pyyaml==3.12',
